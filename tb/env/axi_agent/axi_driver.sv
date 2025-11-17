1c68c84586ea72b98f193801a290e5aaff50f3df
Aashish Pandey <pandeyaashish100@gmail.com>
2025-11-17 18:27:23 -0500

    Implementation of axi_driver

--- tb/env/axi_agent/axi_driver.sv
+++ tb/env/axi_agent/axi_driver.sv
@@ -28,13 +28,84 @@ class axi_driver extends uvm_driver #(axi_seq_item);
         end
     endtask
 
+    
+
+
     //placeholder tasks (TO BE IMPLEMENTED)
     task send_write(axi_seq_item req);
-        //TODO: add handshake logic
+        `uvm_info("AXI_DRV",
+                    $sformatf("WRITE: addr=0x%0h data=0x%0h",
+                    req.addr, req.wdata),
+                    UVM_MEDIUM)
+        //write address channel (AW)
+        vif.AWADDR <= req.addr;
+        vif.AWVALID <= 1;
+
+        @(posedge vif.ACLK);
+        wait(vif.AWREADY == 1);
+
+        //Address accepted
+        vif.AWVALID <= 0;
+
+        //write data channel (W)
+        vif.WDATA <= req.wdata;
+        vif.WSTRB <= 4'hF; //full write
+        vif.WVALID <= 1;
+
+        @(posedge vif.ACLK);
+        wait(vif.WREADY == 1);
+
+        //Data accepted
+        vif.WVALID <= 0;
+
+        //write response
+        vif.BREADY <= 1;
+
+        @(posedge vif.ACLK);
+        wait(vif.BREADY == 1);
+
+        if(vif.BRESP != 0)
+            `uvm_error("AXI_WRITE", "BRESP not OKAY")
+        
+        vif.BREADY <= 0;
+
+
     endtask
 
     task send_read(axi_seq_item req);
-        //TODO: add handshake logic
+        `uvm_info("AXI_DRV", 
+                    $sformatf("READ: addr=0x%0h", req.addr),
+                    UVM_MEDIUM)
+        
+        //read address channel (AR)
+        vif.ARADDR <= req.addr;
+        vif.ARVALID <= 1;
+
+        @(posedge vif.ACLK);
+        wait(vif.ARREADY == 1);
+
+        vif.ARVALID <= 0;
+
+        //read response channel(R)
+
+        vif.RREADY <= 1;
+
+        @(posedge vif.ACLK);
+        wait(vif.RVALID == 1);
+
+        req.rdata = vif.RDATA;
+
+        `uvm_info("AXI_DRV",
+                    $sformatf("READ DATA @0x%0h = 0x%0h", 
+                    req.addr, req.rdata), 
+                    UVM_LOW)
+        
+        if(vif.RRESP != 0)
+            `uvm_error("AXI_READ", "RRESP not OKAY")
+        
+        vif.RREADY <= 0;
+
+
     endtask
 
 endclass
