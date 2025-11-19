64a9cb634accd98faf8822a579bbcda23ebcdc24
Aashish Pandey <pandeyaashish100@gmail.com>
2025-11-19 18:41:56 -0500

    added xrun.f file

---
+++ sim/xrun.f
@@ -0,0 +1,10 @@
+//RTL files
+../rtl/axi4lite_if.sv
+../rtl/timer_core.sv 
+../rtl/timer_regs.sv 
+../rtl/top_timer.sv 
+
+//testbench files
+../tb/tb_pkg.sv 
+../tb/top_tb.sv
+
--- tb/env/timer_env.sv
+++ tb/env/timer_env.sv
@@ -6,14 +6,25 @@ class timer_env extends uvm_env;
 
     `uvm_component_utils(timer_env)
 
-    function new(string name = "timer_env", util_component parent = null);
+    function new(string name = "timer_env", uvm_component parent = null);
         super.new(name, parent);
     endfunction
 
     function void build_phase(uvm_phase phase):
+        super.build_phase(phase);
+
         agent = axi_agent::type_id::create("agent", this);
-        sb = time_scoreboard::type_id::create("sb", this);
+        sb = timer_scoreboard::type_id::create("sb", this);
         cov = timer_cov::type_id::create("cov", this);
     endfunction
+
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+
+        agent.mon.ap.connect(sb.monitor_ap);
+
+        agent.mon.ap.connect(cov.monitor_ap);
+    endfunction
+    
 
 endclass
